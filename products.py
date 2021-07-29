e454cbf95df88ead5a2147ae0e3e519be07540ac
travor1229 <86059172+travor1229@users.noreply.github.com>
2021-07-29 21:17:54 +0800

    Add files via upload

--- products.py
+++ products.py
@@ -4,10 +4,13 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格：')
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p)
+    products.append([name, price])
 print(products)
 
-products[0][0]
+for p in products:
+    print(p[0], '的價格是', p[1])
+
+
+with open('products.txt', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
